ab15bbf0099e1254ccb59d6c2ede66874b20861e
Peter Brumblay <pbrumblay@users.noreply.github.com>
2016-03-15 14:16:04 -0600

    removing fat arrow to conform to ES5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var uuid = require('node-uuid');
 
-var srv = http.createServer( (req, res) => {
+var srv = http.createServer( function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(uuid.v4());
 });
